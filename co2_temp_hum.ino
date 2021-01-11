c484161873e5fab2a94aee43a692caf3b6b41b45
sikisya <sikisya@users.noreply.github.com>
2021-01-11 20:48:40 +0900

    feat: GUI作成
    - OLEDライブラリーをu8g2に変更
    - GUI用の画像を追加(BMP, BMPを変換したXBM)
    - 更新間隔を5秒に変更
    - 2000ppmを超えた際に色反転するように変更

--- co2_temp_hum.ino
+++ co2_temp_hum.ino
@@ -1,12 +1,14 @@
 #include <SoftwareSerial.h>
-#include "DHT.h"
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include <DHT.h>
+#include <U8g2lib.h>
+#include "resource.h"
 
 // PIN define
 #define MHZ19B_RX_PIN 0
 #define MHZ19B_TX_PIN 1
 #define DHT_DATA_PIN 2
+#define OLED_SDA_PIN 4
+#define OLED_SCL_PIN 5
 #define CALIBRATION_PIN 6
 
 #define DHT_TYPE DHT22
@@ -30,7 +32,7 @@ DHT dht(DHT_DATA_PIN, DHT_TYPE);
 SoftwareSerial MH_Z19B(MHZ19B_RX_PIN, MHZ19B_TX_PIN);
 
 // OLED Display
-Adafruit_SSD1306 display = Adafruit_SSD1306(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C display(U8G2_R0, OLED_SCL_PIN, OLED_SDA_PIN);
 
 void calibrate_zero_point() {
   MH_Z19B.write(set_zero, sizeof(set_zero));
@@ -40,24 +42,21 @@ void calibrate_zero_point() {
 void setup() {
   MH_Z19B.begin(9600);
   Serial.begin(9600);
-
-  display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDRESS);
-  display.display();
-  delay(1000);
-  display.clearDisplay();
-  display.display();
-  
-  display.setTextSize(1);
-  display.setTextColor(SSD1306_WHITE);
-  
   dht.begin();
+
+  display.begin();
+  display.clearBuffer();
+  
+  display.setDrawColor(1);
+  display.drawXBMP(0, 0, TITLE_LOGO_WIDTH, TITLE_LOGO_HEIGHT, title_logo);
+  display.sendBuffer();
+  
+  delay(2000);
 
   attachInterrupt(CALIBRATION_PIN, calibrate_zero_point, RISING);
 }
 
 void loop() {
-  delay(2000);
-
   // Get CO2 
   byte responseMessage[9] = {0};
   MH_Z19B.write(request_co2, sizeof(request_co2));
@@ -74,22 +73,57 @@ void loop() {
   Serial.print("ppm, Hum: ");
   Serial.print(humidity);
   Serial.print("%, Temp: ");
-  //Serial.print("Temp: ");
   Serial.print(temperature);
   Serial.println("℃");
 
   // OLED output
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  String temp_val = " Temp: ";
-  String hum_val =  " Hum:  ";
-  String co2_val =  " CO2:  ";
-  temp_val.concat(temperature);
-  hum_val.concat(humidity);
-  co2_val.concat(ppm);
-  display.println("--- Sensor ---");
-  display.println(temp_val);
-  display.println(hum_val);
-  display.println(co2_val);
-  display.display();
+  {
+    display.clearBuffer();
+
+    String temp_val(temperature);
+    String hum_val(humidity);
+    char co2_val[6];
+    sprintf(co2_val, "%5d", ppm);
+    
+    // alert
+    if (ppm > 2000) {
+      display.setDrawColor(1);
+      display.drawBox(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+      display.setDrawColor(0);
+    }
+    else {
+      display.setDrawColor(1);
+    }
+
+    // CO2
+    display.setFont(u8g2_font_lastapprenticebold_tr);
+    display.drawStr(0, 26, "CO2:");
+    display.setFont(u8g2_font_logisoso26_tn);
+    display.drawStr(24, 26, co2_val);
+    display.setFont(u8g2_font_mercutio_basic_nbp_t_all);
+    display.drawStr(109, 24, "ppm");
+
+    // hr
+    display.drawHLine(1, 28, SCREEN_WIDTH);
+
+    // Temp & Hum
+    {
+      int offset_x_temp_hum = 57;
+      display.setFont(u8g2_font_logisoso16_tn);
+      
+      // Temp
+      display.drawXBMP(0 + offset_x_temp_hum, 30, THERMOMETER_WIDTH, THERMOMETER_HEIGHT, thermometer_icon);
+      display.drawStr(15 + offset_x_temp_hum, 46, temp_val.c_str());
+      display.drawXBMP(62 + offset_x_temp_hum, 36, CELSIUS_ICON_WIDTH, CELSIUS_ICON_HEIGHT, celsius_icon);
+  
+      // Hum
+      display.drawXBMP(0 + offset_x_temp_hum, 48, HYGROMETER_WIDTH, HYGROMETER_HEIGHT, hygrometer_icon);
+      display.drawStr(15 + offset_x_temp_hum, 64, hum_val.c_str());
+      display.drawXBMP(62 + offset_x_temp_hum, 54, PERCENT_ICON_WIDTH, PERCENT_ICON_HEIGHT, percent_icon);
+    }
+    
+    display.sendBuffer();
+  }
+  
+  delay(5000);
 }
